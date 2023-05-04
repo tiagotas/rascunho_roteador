a58b9e6227c111f0bc6c3e0980f398f608bc986e
Tiago A. Silva <tiago@tiago.blog.br>
2023-05-04 14:01:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 include "PessoaController.php";
 include "Router.php";
 
-$rota = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
 Router::register(['/pessoa' => 'PessoaController::index']);
 Router::register(['/pessoa/form' => 'PessoaController::form']);
 
-
-Router::get($rota);
+Router::navigate();
 
